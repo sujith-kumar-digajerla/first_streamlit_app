62fa557dfba6c00be1a0d9ca668bb347e5b0b71e
Sujith Digajerla <81622472+sujith-kumar-digajerla@users.noreply.github.com>
2023-04-12 15:57:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title('Welcome to my data application')
+st.title('Welcome to Diner app')
+st.header('Diner Menu')
+st.text('Oat meal')
+st.text('banana smoothie')
+st.text('boiled eggs')
