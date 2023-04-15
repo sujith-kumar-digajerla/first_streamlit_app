2e4b66ac0e3612a32e7fc91dc7a2c7fb5e9217c6
Sujith Digajerla <81622472+sujith-kumar-digajerla@users.noreply.github.com>
2023-04-16 00:31:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import snowflake.connector
+import pandas
+import requests
+from urllib.error import URLError
 
 streamlit.title('Diner app')
 streamlit.header('Breakfast Menu')
@@ -10,7 +13,7 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'] )
@@ -19,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-import requests
+
 
 streamlit.header("Fruityvice Fruit Advice!")
 
